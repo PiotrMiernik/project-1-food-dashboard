f1a10749fe1d1afb281e42f3a75b174f9c6e4a21
Piotr Miernik <miernik.piotr@gmail.com>
2025-07-15 15:47:09 +0200

    Add python-dotenv to requirements for GitHub Actions

--- src/helpers/init_datawarehouse.py
+++ src/helpers/init_datawarehouse.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 from dotenv import load_dotenv
 from src.helpers.db_utils import get_db_connection
 
